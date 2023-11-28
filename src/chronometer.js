39c46c9d673860e08bedfc84d284e277e8c4e7b3
Marine Le Cleach <lecleach.m@gmail.com>
2023-11-28 13:58:29 +0100

    iteration 1,2,3,4

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,18 +1,36 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    // ... your code goes here.
+
+    
+     setInterval( () => {
+      printTimeCallback;
+      this.currentTime++;
+    }, 1000);
+  
   }
+
+  /*printTimeCallback(){
+
+  }*/
+  
 
   getMinutes() {
     // ... your code goes here
+
+  return Math.floor(this.currentTime /60);
   }
 
   getSeconds() {
     // ... your code goes here
+    return Math.floor(this.currentTime % 60);
+
   }
 
   computeTwoDigitNumber(value) {
